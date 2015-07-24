3263ae2e2d27dc2cdbe7fcae4cfb542c62fcc876
vagrant <michael@votemike.co.uk>
2015-07-24 16:11:32 +0000

    Updating redirect only if length doesn't go up that high

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,9 @@ class HomeController extends Controller
     public function index(Request $request, $length = 1) {
         $users = TwitterUser::whereStatus(TwitterAccountStatus::FREE)->whereRaw('LENGTH(username) = ?', [$length])->get();
         if($users->isEmpty() && $length > 1) {
-            return redirect('/');
+            if(TwitterUser::whereRaw('LENGTH(username) = ?', [$length])->count() == 0) {
+                return redirect('/');
+            }
         }
         $lengths = TwitterUser::select(DB::raw('CHAR_LENGTH(username) as length'))->orderBy('length')->groupBy('length')->get()->pluck('length')->toArray();
         return view('home')->withChunks($users->chunk(12))->withLengths($lengths)->withLength((int)$length);
